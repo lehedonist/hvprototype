fb12ec7f75ea542a24800039f1551244409054ba
Yap Zhi Jun <yapzhijun@Zhi-Jun.local>
2016-03-11 09:47:29 +0800

    Updated Settings

--- hvprototype/settings.py
+++ hvprototype/settings.py
@@ -134,7 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'userprofile/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS =  os.path.join(BASE_DIR, 'userprofile/static')
 SITE_ID = 1
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 MEDIA_URL = '/media/'
